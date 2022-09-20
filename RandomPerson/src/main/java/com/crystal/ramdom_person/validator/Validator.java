bea0b95ad77c50d92085d66a45cdc895e600e94c
Indrit Vaka <indrit.vaka@crystal-system.eu>
2022-09-20 10:25:36 +0200

    Code clean Up

--- RandomPerson/src/main/java/com/crystal/ramdom_person/validator/Validator.java
+++ RandomPerson/src/main/java/com/crystal/ramdom_person/validator/Validator.java
@@ -1,14 +1,10 @@
 package com.crystal.ramdom_person.validator;
 
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Validator {
     public static boolean validEmail(String email) {
-        Pattern pattern = Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
-        Matcher m = pattern.matcher(email);
-
-        return m.find();
+        return Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE).matcher(email).find();
     }
 
     public static boolean isValidNumber(String number) {
