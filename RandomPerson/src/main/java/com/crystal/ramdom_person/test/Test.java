4366fb6ead05edcd8e89b9c47a855c323582385c
Indrit-Vaka-w <109148111+Indrit-Vaka-w@users.noreply.github.com>
2022-09-25 21:41:10 +0200

    Update Test.java

--- RandomPerson/src/main/java/com/crystal/ramdom_person/test/Test.java
+++ RandomPerson/src/main/java/com/crystal/ramdom_person/test/Test.java
@@ -1,6 +1,7 @@
 package com.crystal.ramdom_person.test;
 
 public class Test {
+    
     public static void main(String[] args) {
 //        Test test = new Test();
 //        test.setPeopleFilePath();
