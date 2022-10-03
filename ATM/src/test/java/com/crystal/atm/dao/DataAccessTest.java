26c872bbcafa8f4509d4af435a3bb166f3743f79
Indrit Vaka <indrit.vaka@crystal-system.eu>
2022-10-04 01:32:36 +0200

    Updating file

--- ATM/src/test/java/com/crystal/atm/dao/DataAccessTest.java
+++ ATM/src/test/java/com/crystal/atm/dao/DataAccessTest.java
@@ -3,14 +3,13 @@ package com.crystal.atm.dao;
 import com.crystal.atm.model.user.User;
 import org.testng.annotations.Test;
 
-import java.util.List;
+import java.util.Map;
 
 public class DataAccessTest {
     DataAccess dataAccess = new DataFromMemory();
     @Test
     public void testGetPeople() {
-        List<User> people = dataAccess.getUsers();
-
+        Map<Integer, User> people = dataAccess.getUsers();
     }
 
     @Test
