c0839ed6bd84580d634f172527cd5ba7bf832e2e
Indrit-Vaka-w <109148111+Indrit-Vaka-w@users.noreply.github.com>
2022-10-19 17:50:58 +0200

    Testing

--- Hangman/src/test/java/com/crystal/hangman/service/GameServiceTest.java
+++ Hangman/src/test/java/com/crystal/hangman/service/GameServiceTest.java
@@ -1,40 +1,69 @@
 package com.crystal.hangman.service;
 
+import com.crystal.hangman.ai.HangmanBoot;
+import com.crystal.hangman.dao.user.UserDataAccess;
+import com.crystal.hangman.dao.user.UserDataAccessFromProperties;
+import com.crystal.hangman.dao.word.WordDataAccess;
+import com.crystal.hangman.dao.word.WordDataAccessFromFile;
+import com.crystal.hangman.model.GameData;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class GameServiceTest {
+    private final UserDataAccess userDataAccess = new UserDataAccessFromProperties();
+    private final WordDataAccess wordDataAccess = new WordDataAccessFromFile();
+    private final GameService gameService = new GameService(wordDataAccess, userDataAccess);
+    private final HangmanBoot hangmanBoot = new HangmanBoot(gameService);
+    String word = "brick";
 
     @Test
-    void getNextLevel() {
+    void testDashedWordLength() {
+        int actual = gameService.getDashedWord(word).length();
+        Assertions.assertEquals(word.length(), actual);
+
     }
 
     @Test
-    void getDefinition() {
+    void testDashedWord() {
+        StringBuilder actual = gameService.getDashedWord(word);
+        Assertions.assertEquals("-----", actual.toString());
     }
 
     @Test
-    void replaceLettersOld() {
-    }
-
-    @Test
-    void replaceLetters() {
+    void testReplaceLetter() {
+        StringBuilder dashedWord = gameService.getDashedWord(word);
+        String actual = gameService.replaceLetters(word, dashedWord, 'b').toString();
+        Assertions.assertEquals("b----", actual);
     }
 
     @Test
     void hasWon() {
-    }
-
-    @Test
-    void resetGame() {
+        //entire word is found
+        Assertions.assertTrue(gameService.hasWon(word, new StringBuilder(word)));
+        //only a part of word is found
+        Assertions.assertFalse(gameService.hasWon(word, new StringBuilder("br---")));
     }
 
     @Test
     void hasLose() {
+
+        GameData gameData = new GameData();
+        //Mistakes by default are 0, Haven't loosen yet
+        Assertions.assertFalse(gameService.hasLose(gameData));
+
+        //Making the user to lose
+        gameData.setMistakes(gameData.getAllowedMistakes());
+
+        Assertions.assertTrue(gameService.hasLose(gameData));
     }
 
     @Test
     void isUsedLetter() {
+        GameData gameData = new GameData();
+        //By default, there are no used letters at all
+        Assertions.assertFalse(gameService.isUsedLetter(gameData, 'a'));
+        char letter = 'b';
+        gameService.addUsedLetter(gameData,letter);
+        Assertions.assertTrue(gameService.isUsedLetter(gameData,letter));
     }
 }
--- Hangman/src/test/java/com/crystal/hangman/string_joing/TestStringJoin.java
+++ Hangman/src/test/java/com/crystal/hangman/string_joing/TestStringJoin.java
@@ -1,4 +1,13 @@
 package com.crystal.hangman.string_joing;
 
-public class TestStringJoing {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+ class TestStringJoin {
+    @Test
+     void testStringJoin() {
+        String[] data = {"indrit", "vaka"};
+        String actual = String.join(",", data);
+        Assertions.assertEquals("indrit,vaka", actual);
+    }
 }
